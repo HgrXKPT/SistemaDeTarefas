cc82d1e3f7d0cc4fa61ac31663f6a6a5150e3fdb
Higor <higorestevao02@hotmail.com>
2025-02-27 22:11:05 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Models/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace SistemaDeTarefas.Models
+{
+    class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+            "Server=Higor;Database=SistemaDeTarefas;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pessoa>()
+             .HasOne(p => p.Credenciais)
+             .WithOne(c => c.Pessoa)
+             .HasForeignKey<Credenciais>(c => c.PessoaId);
+
+            modelBuilder.Entity<Tarefas>()
+                 .HasOne(t => t.Pessoa)
+                 .WithMany(p => p.Tarefas)
+                 .HasForeignKey(p => p.PessoaId);
+                
+        }
+    }
+}
---
+++ SistemaDeTarefas/Models/Tarefas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeTarefas.Models
+{
+    class Tarefas
+    {
+        public int Id { get; set; }
+        public string NomeTarefa { get; set; }
+        public DateTime DiaCriacao { get; set; }
+
+        public TimeSpan TempoEstimado { get; set; }
+
+        public int PessoaId { get; set; }
+
+        public Pessoa Pessoa { get; set; }
+    }
+}
